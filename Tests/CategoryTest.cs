5184197e0643ca34bbf25048d17bb7d4739b9f5a
Carl Egbert and Matthew Reyes <carl+matthew@>
2016-07-20 09:37:55 -0700

    Create Category class and unit tests

---
+++ Tests/CategoryTest.cs
@@ -0,0 +1,89 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace RecipeBox
+{
+  public class CategoryTest : IDisposable
+  {
+    private List<Ingredient> ingredients = new List<Ingredient> {};
+    private string recipeName = "chocolate chip cookies";
+    private string instructions = "1. Mix Ingredients. 2. Bake for 15 minutes";
+    public void Dispose()
+
+    {
+      Category.DeleteAll();
+    }
+
+    public CategoryTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=recipe_box_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Test_DatabaseEmptyAtFirst()
+    {
+      //Arrange, Act
+      int result = Category.GetAll().Count;
+
+      //Assert
+      Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Test_EqualOverrideTrueForSameDescription()
+    {
+      //Arrange, Act
+      Category firstCategory = new Category("Desserts");
+      Category secondCategory = new Category("Desserts");
+
+      //Assert
+      Assert.Equal(firstCategory, secondCategory);
+    }
+
+    [Fact]
+    public void Test_Save()
+    {
+      //Arrange
+      Category testCategory = new Category("Desserts");
+      testCategory.Save();
+      //Act
+      List<Category> result = Category.GetAll();
+      List<Category> testList = new List<Category>{testCategory};
+
+      //Assert
+      Assert.Equal(testList, result);
+    }
+
+    [Fact]
+    public void Test_FindFindsCategoryInDatabase()
+    {
+      //Arrange
+      Category testCategory = new Category("Desserts");
+      testCategory.Save();
+
+      //Act
+      Category result = Category.Find(testCategory.GetId());
+
+      //Assert
+      Assert.Equal(testCategory, result);
+    }
+
+    [Fact]
+    public void Test_DeleteDeletesCategoryFromDatabase()
+    {
+      Category testCategory = new Category("Desserts");
+      testCategory.Save();
+
+      //Act
+      testCategory.Delete();
+      List<Category> allCategorys = Category.GetAll();
+
+      //Assert
+      Assert.Equal(0, allCategorys.Count);
+    }
+
+  }
+}
